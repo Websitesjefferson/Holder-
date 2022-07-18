cf4df47868865ebf95fe520253bae80546214cd5
Jefferson Borges <105390944+Websitesjefferson@users.noreply.github.com>
2022-07-18 20:07:03 -0300

    Update index.jsx

--- src/component/faq/index.jsx
+++ src/component/faq/index.jsx
@@ -9,12 +9,12 @@ function Foote() {
                 <h1>7 DIAS SEM COMPROMISSO</h1>
 
                 <h4>
-                    Adiquirindo um dos nossos planos, voçè nos auxilia no crecimento
-                    e desenvolvimento desta plataforma, alé de receber vantagens exclusivas!
+                   Adquirindo um dos nossos planos, você nos auxilia no crecimento
+                   e desenvolvimento desta plataforma, além de receber vantagens exclusivas!
                 </h4>
 
                 <p>
-                    Comprando seu primeiro plano voçé garante ate 7  dias para experimentar,
+                    Comprando seu primeiro plano você garante ate 7 dias para experimentar,
                     podendo nesse período cancelar e pedir seu reembolso diretamente na plataforma,
                     sem complicações!
                 </p>
